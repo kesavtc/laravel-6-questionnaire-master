2718e22e166ee9196dd811e55ebd75ca6e80508d
kesavan <kesavtc@gmail.com>
2020-06-15 20:38:54 +0530

    my first commit for this demo project

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $questionnaires = auth()->user()->questionnaires;
+        return view('home',compact('questionnaires'));
+    }
+
+    public function getroutes()
+    {
+        $contents = File::get('../routes/web.php');
+        echo $contents;
+    }
+}
---
+++ resources/views/survey/show.blade.php
@@ -0,0 +1,97 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+
+            <h1>{{ $questionnaire->title }}</h1>
+
+            <form action="/surveys/{{ $questionnaire->id }}-{{ Str::slug($questionnaire->title) }}" method="post">
+                @csrf
+
+                <?php $flag =1; ?>
+                @forelse($questionnaire->questions as $key => $question)
+                <div class="card mt-4">
+                    <div class="card-header"><strong>{{ $key + 1 }}</strong> {{ $question->question }}</div>
+
+                    <div class="card-body">
+                        @error('responses.'.$key.'.answer_id')
+                        <small class="text-danger">{{ $message }}</small>
+
+                        @enderror
+                        <ul class="list-group">
+                            @foreach($question->answers as $answer)
+                            <label for="answer{{ $answer->id }}">
+                                <li class="list-group-item">
+                                    <input type="radio" name="responses[{{ $key }}][answer_id]" id="answer{{ $answer->id }}"
+                                    {{ old('responses.'.$key.'.answer_id') == $answer->id ? 'checked' : '' }}
+                                class="mr-2" value="{{ $answer->id }}">
+                                    {{ $answer->answer }}
+                                    <input type="hidden" name="responses[{{ $key }}][question_id]" value="{{ $question->id }}">
+                                </li>
+                            </label>
+
+                            @endforeach
+                        </ul>
+                    </div>
+
+                </div>
+
+                @empty
+                <div class="card mt-4">
+                    <div class="card-body">No questions found </div>
+                </div>
+                <?php $flag = 0; ?>
+
+                @endforelse
+
+
+
+
+            <?php if($flag) { ?>
+
+            <div class="card mt-4">
+                <div class="card-header">Your Information</div>
+
+                <div class="card-body">
+
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input name="survey[name]" type="text" class="form-control" id="name" aria-describedby="nameHelp" placeholder="Enter name">
+                            <small id="nameHelp" class="form-text text-muted">Hello what's your name</small>
+
+                            @error('name')
+                            <small class="text-danger">{{ $message }}</small>
+                            @enderror
+
+                        </div>
+
+                          <div class="form-group">
+                              <label for="email">Email</label>
+                              <input name="survey[email]" type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+                              <small id="emailHelp" class="form-text text-muted">Your Email Please</small>
+
+                              @error('email')
+                              <small class="text-danger">{{ $message }}</small>
+                              @enderror
+
+                         </div>
+
+
+                </div>
+            </div>
+
+            <div>
+                <button class="btn btn-dark" type="submit">Complete Survey</button>
+            </div>
+
+        </form>
+
+    <?php } ?>
+
+
+        </div>
+    </div>
+</div>
+@endsection
